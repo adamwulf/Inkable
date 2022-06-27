6d25e515aee6366b6269da0255463f6128d84f27
Adam Wulf <adam.wulf@gmail.com>
2022-06-27 13:50:37 -0500

    Prepping to show events in a list, and allow tapping each event to move step by step

---
+++ Example/Example/EventsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.swift
+//  Example
+//
+//  Created by Adam Wulf on 6/27/22.
+//
+
+import Foundation
+import UIKit
+
+class EventsViewController: UITableViewController {
+
+}
--- Example/Example/SplitViewController.swift
+++ Example/Example/SplitViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class SplitViewController: UISplitViewController {
 
     private var settingsViewController: SettingsViewController? {
-        guard let nav = self.viewController(for: .primary) as? UINavigationController else { return nil }
+        guard let nav = self.viewController(for: .supplementary) as? UINavigationController else { return nil }
         return nav.viewControllers.first as? SettingsViewController
     }
 
     private var debugViewController: DebugViewController? {
         return self.viewController(for: .secondary) as? DebugViewController
+    }
+
+    private var eventsViewController: EventsViewController? {
+        guard let nav = self.viewController(for: .primary) as? UINavigationController else { return nil }
+        return nav.viewControllers.first as? EventsViewController
     }
 
     override func viewDidLoad() {
