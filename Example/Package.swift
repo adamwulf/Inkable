f7c78f8c0ef59767d3fdb87ae74119a6467cfaf3
Adam Wulf <adam.wulf@gmail.com>
2022-07-04 15:32:10 -0500

    Add a Package.swift into the Example folder so that it doesn't show in Xcode when opening only the Inkable package

---
+++ Example/Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Example"
+}
