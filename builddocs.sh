9e640123e0f8b910a1bfca7fe39fe46b34d45da7
Adam Wulf <adam.wulf@gmail.com>
2022-07-10 22:44:00 -0500

    Build docs and install locally as well as regenerate docs folder

--- builddocs.sh
+++ builddocs.sh
@@ -1,1 +1,4 @@
 xcodebuild docbuild -scheme Inkable -destination generic/platform=iOS OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path Inkable --output-path docs"
+xcodebuild docbuild -scheme Inkable -destination generic/platform=iOS OTHER_DOCC_FLAGS="--output-path Inkable.doccarchive"
+open Inkable.doccarchive
+rm -rf Inkable.doccarchive
