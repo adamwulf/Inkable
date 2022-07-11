f3913f28dd937cdaf8f7ecb117cf14bafc9de76d
Adam Wulf <adam.wulf@gmail.com>
2022-07-10 23:29:04 -0500

    Adding file for root documentation. adding github workflow

---
+++ Sources/Inkable/Documentation.docc/Inkable.md
@@ -0,0 +1,3 @@
+# ``Inkable``
+
+Basic description.
--- builddocs.sh
+++ builddocs.sh
@@ -1,4 +1,3 @@
 xcodebuild docbuild -scheme Inkable -destination generic/platform=iOS OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path Inkable --output-path docs"
-xcodebuild docbuild -scheme Inkable -destination generic/platform=iOS OTHER_DOCC_FLAGS="--output-path Inkable.doccarchive"
-open Inkable.doccarchive
-rm -rf Inkable.doccarchive
+xcodebuild docbuild -scheme Inkable -destination generic/platform=iOS OTHER_DOCC_FLAGS="--output-path .build/Inkable.doccarchive"
+open .build/Inkable.doccarchive
