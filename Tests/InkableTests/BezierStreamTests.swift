b08ddc535f780e3239df9882b68e881fe2c5e0eb
Adam Wulf <adam.wulf@gmail.com>
2022-07-30 22:47:13 +0000

    Adding test for enable/disable bezier stream

--- Tests/InkableTests/BezierStreamTests.swift
+++ Tests/InkableTests/BezierStreamTests.swift
@@ -130,4 +130,31 @@ class BezierStreamTests: XCTestCase {
         XCTAssertEqual(bezierStream.paths.count, 1)
         XCTAssertEqual(bezierStream.paths[0].elementCount, allElementCount)
     }
+
+    func testIsEnabledFlag() throws {
+        let completeEvents = [Event(x: 100, y: 100),
+                              Event(x: 110, y: 110),
+                              Event(x: 120, y: 160),
+                              Event(x: 130, y: 120),
+                              Event(x: 140, y: 120),
+                              Event(x: 150, y: 110)]
+        let points = Polyline.Point.newFrom(completeEvents)
+        let line = Polyline(points: points)
+        let polylineOutput = PolylineStream.Produces(lines: [line], deltas: [.addedPolyline(index: 0)])
+
+        let bezierStream = BezierStream(smoother: AntigrainSmoother())
+        bezierStream.isEnabled = false
+
+        var bezierOutput = bezierStream.produce(with: polylineOutput)
+
+        XCTAssert(bezierOutput.paths.isEmpty)
+        XCTAssert(bezierOutput.deltas.isEmpty)
+
+        bezierStream.isEnabled = true
+
+        bezierOutput = bezierStream.produced!
+
+        XCTAssert(polylineOutput.lines[0] == bezierOutput.paths[0])
+        XCTAssertEqual(bezierOutput.deltas[0], .addedBezierPath(index: 0))
+    }
 }
